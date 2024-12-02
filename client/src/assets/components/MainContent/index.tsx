cdf5cf7600741241d80a1f177a39cef9caccb62c
Попов Матвей Сергеевич <pms@rkbglobus.ru>
2024-12-02 16:32:40 +0300

    tour-visit design,seo,styles v 2.0.1

--- client/src/assets/components/MainContent/index.tsx
+++ client/src/assets/components/MainContent/index.tsx
@@ -4,11 +4,15 @@ import YouTubeHref from "../../images/social/youtube.png";
 import InstHref from "../../images/social/instagram.png";
 import TgHref from "../../images/social/telegramm.png";
 import VkHref from "../../images/social/vk.png";
+import whatsappIcon from "../../images/icons/whatsapp.png";
+import requestCall from "../../images/icons/call.png";
+import lupe from "../../images/icons/lupe.png";
+import phone from "../../images/icons/phone.png";
+import user from "../../images/icons/user.png";
 import { ImageSlider } from "../ImageSlider";
 import "./css/style.css";
-import "./css/checkbox.css";
-import "./css/svg_mark.css";
-
+// import "./css/checkbox.css";
+// import "./css/svg_mark.css";
 
 export function MainContent() {
     const [checkboxValue] = useState(true);
@@ -22,38 +26,41 @@ export function MainContent() {
                 <div className="content-container">
                     {/* Шапка */}
                     <div className="header_container">
-                        <h2>Мультибрендовая сеть турагентств</h2>
+                        {/* <h2>Мультибрендовая сеть турагентств</h2>
                         <h1>
                             <a href="https://tourstore.online/">Tourstore.online</a>
-                        </h1>
+                        </h1> */}
                         <a href="https://tourstore.online/"><img src={logoHref} alt="Логотип" /></a>
 
                     </div>
                     {/* Блок с checkbox-сами */}
                     <div className="checkbox-container">
                         <div className="checkbox-container_item">
-                            <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                            {/* <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
                                 <circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none" />
                                 <path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
-                            </svg>
+                            </svg> */}
+                            <input type="checkbox" defaultChecked={checkboxValue} disabled  />
                             <li>Более 40 тысяч довольных туристов</li>
                             {/* <input type="checkbox" id="switch" className="checkbox" defaultChecked={checkboxValue} />
                             <label htmlFor="switch"></label> */}
                         </div>
                         <div className="checkbox-container_item">
-                            <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                            {/* <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
                                 <circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none" />
                                 <path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
-                            </svg>
+                            </svg> */}
+                            <input type="checkbox" defaultChecked={checkboxValue} disabled />
                             <li>Опыт в туризме более 12 лет</li>
                             {/* <input type="checkbox" id="switch" className="checkbox" defaultChecked={checkboxValue} />
                             <label htmlFor="switch"></label> */}
                         </div>
                         <div className="checkbox-container_item">
-                            <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                            {/* <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
                                 <circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none" />
                                 <path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
-                            </svg>
+                            </svg> */}
+                            <input type="checkbox" defaultChecked={checkboxValue} disabled />
                             <li>На связи 24/7</li>
                             {/* <input type="checkbox" id="switch" className="checkbox" defaultChecked={checkboxValue} />
                             <label htmlFor="switch"></label> */}
@@ -61,46 +68,121 @@ export function MainContent() {
                     </div>
                     {/* Кнопки*/}
                     <div className="buttons-container flex">
-                        <a href="tel:88002220633">Позвонить бесплатно</a>
-                        <a href="https://wa.me/+79308770633">Написать What's up</a>
+                        <a href="tel:88002220633">
+                            <img src={phone} alt="" />
+                            <span>Позвонить бесплатно</span>
+                        </a>
+                    </div>
+                    <div className="green-buttons">
+                        <a href="https://wa.me/+79308770633">
+                            <img src={whatsappIcon} alt="Иконка WhatsApp" />
+                        </a>
+                        <a href="#">
+                            <img src={requestCall} alt="Иконка по запросу звонка" />
+                        </a>
+                    </div>
+                    <div className="buttons-container flex">
+                        <a href="tel:88002220633">
+                            <img src={user} alt="Сохранить контакт" />
+                            <span>Сохранить контакт</span>
+                        </a>
                     </div>
                     {/* Карусель с контактами */}
-                    <div className="contacts-container">
-                        <a href="https://www.youtube.com/@Tourstore.online">
-                            <img src={YouTubeHref} alt="Youtube" />
-                        </a>
-                        <a href="https://vk.com/tourstore.online">
-                            <img src={VkHref} alt="Vk" />
-                        </a>
-                        <a href="https://t.me/TourStore_online">
-                            <img src={TgHref} alt="Tg" />
-                        </a>
-                        <a href="https://www.instagram.com/tourstore.online">
-                            <img src={InstHref} alt="Inst" />
-                        </a>
+                    {/* <div className="contacts-container_text">
+                    </div> */}
+                    <div className="contacts-container_block">
+                        <span>Всё самое выгодное и нтересное в наших сообществах</span>
+                        <div className="contacts-container">
+                            <a href="https://www.youtube.com/@Tourstore.online">
+                                <img src={YouTubeHref} alt="Youtube" />
+                            </a>
+                            <a href="https://vk.com/tourstore.online">
+                                <img src={VkHref} alt="Vk" />
+                            </a>
+                            <a href="https://t.me/TourStore_online">
+                                <img src={TgHref} alt="Tg" />
+                            </a>
+                            <a href="https://www.instagram.com/tourstore.online">
+                                <img src={InstHref} alt="Inst" />
+                            </a>
+                        </div>
                     </div>
                     {/* Кнопки*/}
-                    <div className="find-tours-container flex">
+                    <div className="buttons-container flex">
+                        <a href="tel:88002220633">
+                            <img src={lupe} alt="" />
+                            <span>Найти тур</span>
+                        </a>
+                    </div>
+                    {/* <div className="find-tours-container flex">
                         <a href="https://tourstore.online/Tours/">Найти тур</a>
                         <a href="https://tourstore.online/Tours/">Подписаться на рассылку</a>
-                    </div>
+                    </div> */}
                     {/* Подвал */}
                     <div className="footer-container">
                         <h3>Наши офисы</h3>
+                        <span>У нас три оффлайн офиса, но мы паботаем со всей Россией</span>
+                        <div className="slider-container"><ImageSlider items={[
+                            {
+                                image: './images/SliderHouses/funsun.png',
+                                href: "https://tourstore.online/contacts/kasimov/FunSun/",
+                            }
+                            , {
+                                image: './images/SliderHouses/pegas.png',
+                                href: "https://tourstore.online/contacts/murom/pegas/",
+                            },
+                            {
+                                image: './images/SliderHouses/anex.png',
+                                href: 'https://tourstore.online/contacts/murom/anex/',
+                            }
+                        ]} />
+                        </div>
+                        <div className="dialog">
+                            <img src="./images/icons/avatar.png" alt="" />
+                            <div className="dialog-content">
+                                <span>
+                                    Привет! Меня зовут Ирина Гапич. Я осователь и руководитель TourStore. Обратившись к нам, вы сделали верный выбор.
+                                    Организация отдыха - хоть и приятная, но очень кропотливая работа -
+                                    она не прощает даже мельчайших просчётов, и лучше доверить её тому, кто занимается ей ежедневно и знает обо всех 'подводных камнях' и тонкостях
+                                </span>
+                            </div>
+                        </div>
+                        <h3>Наши специалисты</h3>
+                        {/* <span>У нас три оффлайн офиса, но мы паботаем со всей Россией</span> */}
                         <div className="slider-container">
                             <ImageSlider items={[
                                 {
-                                    image: './images/SliderHouses/third.jpg',
-                                    href: "https://tourstore.online/contacts/kasimov/FunSun/",
+                                    image: './images/staff/79009666505.png',
+                                    href: "https://wa.me/+79009666505",
                                 }
                                 , {
-                                    image: './images/SliderHouses/second.jpg',
-                                    href: "https://tourstore.online/contacts/murom/pegas/",
+                                    image: './images/staff/79009666545.png',
+                                    href: "https://wa.me/+79009666545",
                                 },
                                 {
-                                    image: './images/SliderHouses/first.jpg',
-                                    href: 'https://tourstore.online/contacts/murom/anex/',
-                                }
+                                    image: './images/staff/79028889818.png',
+                                    href: "https://wa.me/+79028889818",
+                                },
+                                {
+                                    image: './images/staff/79028889828.png',
+                                    href: "https://wa.me/+79028889828",
+                                },
+                                {
+                                    image: './images/staff/79107717890.png',
+                                    href: "https://wa.me/+79107717890",
+                                },
+                                {
+                                    image: './images/staff/79107717892.png',
+                                    href: "https://wa.me/+79107717892",
+                                },
+                                {
+                                    image: './images/staff/79157901990.png',
+                                    href: "https://wa.me/+79157901990",
+                                },
+                                {
+                                    image: './images/staff/79511027000.png',
+                                    href: "https://wa.me/+79511027000",
+                                },
                             ]} />
                         </div>
                         <div className="attributes">
