96012ca6bfcfaf6b0521d25465e68f74773f5518
PopovMatvey <matyha550@gmail.com>
2025-01-04 22:49:18 +0300

    tour_qr-visit task/debug add contact v 3.2.3

--- server/app.js
+++ server/app.js
@@ -1,189 +1,189 @@
-// /*Libs*/
-// const cors = require('cors')                                // allow api requests/response
-// const fs = require('fs');                                   // for work with file/dirrectory
-// const express = require('express');                         // api requests lib
-// const path = require('path');                               // for init static directory
-// const { v4 } = require('uuid');                             // generate id
-// const app = express();                                      // app iniy
-// app.use(express.json());                                    // use json for requests
-// app.use(cors());
+/*Libs*/
+const cors = require('cors')                                // allow api requests/response
+const fs = require('fs');                                   // for work with file/dirrectory
+const express = require('express');                         // api requests lib
+const path = require('path');                               // for init static directory
+const { v4 } = require('uuid');                             // generate id
+const app = express();                                      // app iniy
+app.use(express.json());                                    // use json for requests
+app.use(cors());
 
-// /*Varibles*/
-// const PORT_APP = 2006;                      // app port
-// const urlRequest = '/api/contacts';         // url request api
-// const arrayPartnersSlider = getFiles('./static/images/Slider/Parnters/');
-// // const arrayImageSlayderPortfolio = getFiles('./static/media/images/');
-// // const arrayVideosSlayderPortfolio =  getFiles('./static/media/videos/');
-// // const arrayAllMedia = getFiles('./static/media/');
-// const arrayPortfolioSlider = [
-//     'http://z92017tk.beget.tech/static/images/Slider/Portfolio/collection.jpg',
-//     'http://z92017tk.beget.tech/static/images/Slider/Portfolio/double_headed_eagle.jpg',
-//     'http://z92017tk.beget.tech/static/images/Slider/Portfolio/helmet.jpg',
-//     'http://z92017tk.beget.tech/static/images/Slider/Portfolio/military_helmet.jpg',
-//     'http://z92017tk.beget.tech/static/images/Slider/Portfolio/pony.jpg',
-//     'http://z92017tk.beget.tech/static/images/Slider/Portfolio/swan.jpg',
-//     'http://z92017tk.beget.tech/static/images/Slider/Portfolio/trunk.jpg',
-// ];
-// //getFiles('./static/images/Slider/Portfolio/');
-// const arrayStudentsSlider = getFiles('./static/images/Slider/Students/');
-// const arrayVideos = [
-//     'http://z92017tk.beget.tech/static/videos/Slider/Portfolio/flower.mp4',
-//     'http://z92017tk.beget.tech/static/videos/Slider/Portfolio/helmet.mp4',
-//     'http://z92017tk.beget.tech/static/videos/Slider/Portfolio/shoe.mp4',
-//     'http://z92017tk.beget.tech/static/videos/Slider/Portfolio/wheel_rim.mp4',
-// ];
-// //getFiles('./static/videos/Slider/Portfolio/')
-// const arrayYouTubeVideos = [
-//     "QqgBzPfBkj0",
-//     "QqgBzPfBkj1",
-//     "QqgBzPfBkj0",
-//     "QqgBzPfBkj0",
-// ];
+/*Varibles*/
+const PORT_APP = 2006;                      // app port
+const urlRequest = '/api/contacts';         // url request api
+const arrayPartnersSlider = getFiles('./static/images/Slider/Parnters/');
+// const arrayImageSlayderPortfolio = getFiles('./static/media/images/');
+// const arrayVideosSlayderPortfolio =  getFiles('./static/media/videos/');
+// const arrayAllMedia = getFiles('./static/media/');
+const arrayPortfolioSlider = [
+    'http://z92017tk.beget.tech/static/images/Slider/Portfolio/collection.jpg',
+    'http://z92017tk.beget.tech/static/images/Slider/Portfolio/double_headed_eagle.jpg',
+    'http://z92017tk.beget.tech/static/images/Slider/Portfolio/helmet.jpg',
+    'http://z92017tk.beget.tech/static/images/Slider/Portfolio/military_helmet.jpg',
+    'http://z92017tk.beget.tech/static/images/Slider/Portfolio/pony.jpg',
+    'http://z92017tk.beget.tech/static/images/Slider/Portfolio/swan.jpg',
+    'http://z92017tk.beget.tech/static/images/Slider/Portfolio/trunk.jpg',
+];
+//getFiles('./static/images/Slider/Portfolio/');
+const arrayStudentsSlider = getFiles('./static/images/Slider/Students/');
+const arrayVideos = [
+    'http://z92017tk.beget.tech/static/videos/Slider/Portfolio/flower.mp4',
+    'http://z92017tk.beget.tech/static/videos/Slider/Portfolio/helmet.mp4',
+    'http://z92017tk.beget.tech/static/videos/Slider/Portfolio/shoe.mp4',
+    'http://z92017tk.beget.tech/static/videos/Slider/Portfolio/wheel_rim.mp4',
+];
+//getFiles('./static/videos/Slider/Portfolio/')
+const arrayYouTubeVideos = [
+    "QqgBzPfBkj0",
+    "QqgBzPfBkj1",
+    "QqgBzPfBkj0",
+    "QqgBzPfBkj0",
+];
 
-// // console.log(arrayAllMedia);
+// console.log(arrayAllMedia);
 
-// /*Methods*/
-// // get all files in determ directory
-// function getFiles(dir, files_) {
-//     files_ = files_ || [];
-//     var files = fs.readdirSync(dir);
+/*Methods*/
+// get all files in determ directory
+function getFiles(dir, files_) {
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
 
-//     for (var i in files) {
-//         var name = dir + '/' + files[i];
+    for (var i in files) {
+        var name = dir + '/' + files[i];
 
-//         if (fs.statSync(name).isDirectory()) {
-//             getFiles(name, files_);
-//         } else {
-//             files_.push(name);
-//         }
-//     }
+        if (fs.statSync(name).isDirectory()) {
+            getFiles(name, files_);
+        } else {
+            files_.push(name);
+        }
+    }
 
-//     return deletePartString(files_);
-// };
+    return deletePartString(files_);
+};
 
-// function deletePartString(array) {
-//     let returnedArray = [];
+function deletePartString(array) {
+    let returnedArray = [];
 
-//     for (let i = 0; i < array.length; i++) {
-//         returnedArray.push(array[i].replace("./static", ''))
-//     }
+    for (let i = 0; i < array.length; i++) {
+        returnedArray.push(array[i].replace("./static", ''))
+    }
 
-//     return returnedArray;
-// }
+    return returnedArray;
+}
 
-// /*Requests*/
-// //GET
-// // Получить массив путей до изображений партнёров
-// app.get('/api/slider/image/partners', (req, res) => {
-//     res.status(200).json(arrayPartnersSlider);
-// });
+/*Requests*/
+//GET
+// Получить массив путей до изображений партнёров
+app.get('/api/slider/image/partners', (req, res) => {
+    res.status(200).json(arrayPartnersSlider);
+});
 
-// // Получить массив путей до изображений "Портфолио"
-// app.get('/api/slider/image/protfolio', (req, res) => {
-//     res.status(200).json(arrayPortfolioSlider);
-// });
+// Получить массив путей до изображений "Портфолио"
+app.get('/api/slider/image/protfolio', (req, res) => {
+    res.status(200).json(arrayPortfolioSlider);
+});
 
-// // app.get('api/portfolio/images', (req,res)=>{
-// //     res.status(200).json(arrayImageSlayderPortfolio);
-// // })
-
-// // app.get('api/portfolio/videos', (req,res)=>{
-// //     res.status(200).json(arrayVideosSlayderPortfolio);
-// // })
-
-
-// // Получить массив путей до изображений "Учеников"
-// app.get('/api/slider/image/students', (req, res) => {
-//     res.status(200).json(arrayStudentsSlider);
-// });
-
-// // Получить массив путей до ютуб-видео "Портфолио"
-// app.get('/api/slider/video-youtube/portfolio', (req, res) => {
-//     res.status(200).json(arrayYouTubeVideos);
+// app.get('api/portfolio/images', (req,res)=>{
+//     res.status(200).json(arrayImageSlayderPortfolio);
 // })
 
-// // Получить массив путей до ютуб-видео "Портфолио"
-// app.get('/api/slider/video/portfolio', (req, res) => {
-//     res.status(200).json(arrayVideos);
+// app.get('api/portfolio/videos', (req,res)=>{
+//     res.status(200).json(arrayVideosSlayderPortfolio);
 // })
 
-// //POST "CREATE"
-// app.post(`${urlRequest}`, (req, res) => {
-//     const contact = { ...req.body, id: v4(), marked: false }
-//     CONTACTS.push(contact);
-//     res.status(201).json(contact);
-// });
 
-// //DELETE
-// app.delete(`${urlRequest}/:id`, (req, res) => {
-//     CONTACTS = CONTACTS.filter(c =>
-//         c.id != req.params.id
-//     );
-//     res.status(200).json({ message: "Контакт был удалён" });
-// });
+// Получить массив путей до изображений "Учеников"
+app.get('/api/slider/image/students', (req, res) => {
+    res.status(200).json(arrayStudentsSlider);
+});
 
-// //PUT
-// app.put(`${urlRequest}/:id`, (req, res) => {
-//     const idx = CONTACTS.findIndex(c => c.id === req.params.id);
-//     CONTACTS[idx] = req.body;
-//     res.json(CONTACTS[idx]);
-// });
+// Получить массив путей до ютуб-видео "Портфолио"
+app.get('/api/slider/video-youtube/portfolio', (req, res) => {
+    res.status(200).json(arrayYouTubeVideos);
+})
 
-// /*Directory*/
-// // init statics
-// app.use(express.static(path.resolve(__dirname, 'client')));
-// app.use(express.static(path.resolve(__dirname, '.static')));
+// Получить массив путей до ютуб-видео "Портфолио"
+app.get('/api/slider/video/portfolio', (req, res) => {
+    res.status(200).json(arrayVideos);
+})
 
-// // lisening all get requests
-// app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", 'index.html'));
-// });
+//POST "CREATE"
+app.post(`${urlRequest}`, (req, res) => {
+    const contact = { ...req.body, id: v4(), marked: false }
+    CONTACTS.push(contact);
+    res.status(201).json(contact);
+});
 
-// // default massage
-// app.listen(PORT_APP, () => console.log(`Server has been started on port ${PORT_APP}`));
+//DELETE
+app.delete(`${urlRequest}/:id`, (req, res) => {
+    CONTACTS = CONTACTS.filter(c =>
+        c.id != req.params.id
+    );
+    res.status(200).json({ message: "Контакт был удалён" });
+});
 
-// Load the TCP Library
-net = require('net');
+//PUT
+app.put(`${urlRequest}/:id`, (req, res) => {
+    const idx = CONTACTS.findIndex(c => c.id === req.params.id);
+    CONTACTS[idx] = req.body;
+    res.json(CONTACTS[idx]);
+});
 
-// Keep track of the chat clients
-var clients = [];
+/*Directory*/
+// init statics
+app.use(express.static(path.resolve(__dirname, 'client')));
+app.use(express.static(path.resolve(__dirname, '.static')));
 
-// Start a TCP Server
-net.createServer(function (socket) {
+// lisening all get requests
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", 'index.html'));
+});
 
-  // Identify this client
-  socket.name = socket.remoteAddress + ":" + socket.remotePort 
+// default massage
+app.listen(PORT_APP, () => console.log(`Server has been started on port ${PORT_APP}`));
 
-  // Put this new client in the list
-  clients.push(socket);
+// // Load the TCP Library
+// net = require('net');
 
-  // Send a nice welcome message and announce
-  socket.write("Welcome " + socket.name + "\n");
-  broadcast(socket.name + " joined the chat\n", socket);
+// // Keep track of the chat clients
+// var clients = [];
 
-  // Handle incoming messages from clients.
-  socket.on('data', function (data) {
-    broadcast(socket.name + "> " + data, socket);
-  });
+// // Start a TCP Server
+// net.createServer(function (socket) {
 
-  // Remove the client from the list when it leaves
-  socket.on('end', function () {
-    clients.splice(clients.indexOf(socket), 1);
-    broadcast(socket.name + " left the chat.\n");
-  });
+//   // Identify this client
+//   socket.name = socket.remoteAddress + ":" + socket.remotePort 
+
+//   // Put this new client in the list
+//   clients.push(socket);
+
+//   // Send a nice welcome message and announce
+//   socket.write("Welcome " + socket.name + "\n");
+//   broadcast(socket.name + " joined the chat\n", socket);
+
+//   // Handle incoming messages from clients.
+//   socket.on('data', function (data) {
+//     broadcast(socket.name + "> " + data, socket);
+//   });
+
+//   // Remove the client from the list when it leaves
+//   socket.on('end', function () {
+//     clients.splice(clients.indexOf(socket), 1);
+//     broadcast(socket.name + " left the chat.\n");
+//   });
   
-  // Send a message to all clients
-  function broadcast(message, sender) {
-    clients.forEach(function (client) {
-      // Don't want to send it to sender
-      if (client === sender) return;
-      client.write(message);
-    });
-    // Log it to the server output too
-    process.stdout.write(message)
-  }
+//   // Send a message to all clients
+//   function broadcast(message, sender) {
+//     clients.forEach(function (client) {
+//       // Don't want to send it to sender
+//       if (client === sender) return;
+//       client.write(message);
+//     });
+//     // Log it to the server output too
+//     process.stdout.write(message)
+//   }
 
-}).listen(5000);
+// }).listen(5000);
 
-// Put a friendly message on the terminal of the server.
-console.log("Chat server running at port 5000\n");
+// // Put a friendly message on the terminal of the server.
+// console.log("Chat server running at port 5000\n");
